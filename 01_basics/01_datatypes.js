040da3dba6dec668ef334e1c635506b4921e693d
Nilesh-Anjana <nilesh929788@gmail.com>
2025-03-19 07:22:06 +0000

    data types basics

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+//console.log("typeof ....... ")   can be used to find the type
+
+//number   2 to power 53
+//bigint
+//string
+//boolean
+//null     - standalone value  (no value found)  --> type is object
+
+//undefined - value not assigned yet  --> type is undefined
+
+// symbol - it is used for finding the uniqueness (like in react the uniquene components )
+
+//object
