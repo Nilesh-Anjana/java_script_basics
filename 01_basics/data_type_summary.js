f95a7dd9a734666b82cdc14e96ca5d74741e78cc
Nilesh-Anjana <nilesh929788@gmail.com>
2025-03-19 09:11:28 +0000

    peimitive,non premitive

---
+++ 01_basics/data_type_summary.js
@@ -0,0 +1,20 @@
+//**********//primitive data type//***********//
+
+// these are always call by value....means they are passed as a new copy.
+
+//string ,number , boolean , null, undefinefd , symbol , Bigint
+
+
+
+
+
+//*********refrence type******//(non premitive)
+
+//arrays, objects , functions
+
+
+
+
+
+
+
