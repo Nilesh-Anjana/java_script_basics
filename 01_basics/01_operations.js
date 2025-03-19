d3a9f8094cff95dee67a78e6a5d42db353de95a5
Nilesh-Anjana <nilesh929788@gmail.com>
2025-03-19 08:44:17 +0000

    basic operations

---
+++ 01_basics/01_operations.js
@@ -0,0 +1,14 @@
+//console.log(9%10) o/p-> 9
+//console.log(1%10) o/p-> 1
+//console.log(13%3) o/p ->1
+
+//console.log("1"+2);  --->12
+//console.log(1+ "2"); --> 12
+//console.log("1" + 2 + 2);--> 122
+//console.log(1+2+"2");--> 32   ********imp*******
+
+
+//console.log(true)  -->true
+
+//console.log(+true) --> 1
+
