1339831bee69284826bff423d8e483af50e3dc70
Nilesh-Anjana <nilesh929788@gmail.com>
2025-03-19 06:36:03 +0000

    understanding the basics of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const accountId = 144553  //we can't cahnge it
+let accountEmail = "nil@123.com"
+var accountPassword = "12345"  // prefer not to use it due to issue in  block scope and functional scope  means it can be access outside the function or block.
+accountCity = "jaipur"  // it can be modified
